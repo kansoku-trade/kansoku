3e6d4686f05d192f38fdd5b290e7125799003069
Innei <tukon479@gmail.com>
2026-07-23 20:59:58 +0800

    feat(research): wire create into server controller and desktop IPC

--- apps/desktop/src/kernel/ipc/researchIpc.ts
+++ apps/desktop/src/kernel/ipc/researchIpc.ts
@@ -1,11 +1,12 @@
 import { IpcMethod, IpcService } from 'electron-ipc-decorator';
 import type { ResearchApi } from '@kansoku/core/contract/index';
+import { researchCreate } from '@kansoku/core/research/createResearch';
 import { researchService } from '@kansoku/core/research/research.service';
 import { toEnvelope, type WrapEnvelope } from './envelope.js';
 
 export class ResearchIpc
   extends IpcService
-  implements WrapEnvelope<Pick<ResearchApi, 'list' | 'get'>>
+  implements WrapEnvelope<Pick<ResearchApi, 'list' | 'get' | 'create'>>
 {
   static readonly groupName = 'research';
 
@@ -17,5 +18,10 @@ export class ResearchIpc
   @IpcMethod()
   get(input: Parameters<ResearchApi['get']>[0]) {
     return toEnvelope('research.get', () => researchService.get(input));
+  }
+
+  @IpcMethod()
+  create(input: Parameters<ResearchApi['create']>[0]) {
+    return toEnvelope('research.create', () => researchCreate(input));
   }
 }
